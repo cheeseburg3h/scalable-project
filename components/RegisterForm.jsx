896d740cb92acdf85dda07d4ef5a7728322b58d4
justin <110153118+cheeseburg3h@users.noreply.github.com>
2024-06-03 15:37:02 +0700

    push temporary code

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -61,40 +61,49 @@ export default function RegisterForm() {
   };
 
   return (
-    <div className="grid place-items-center h-screen">
-      <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
-        <h1 className="text-xl font-bold my-4">Register</h1>
-
-        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="bg-[#ffffff] p-8 rounded shadow-md w-96">
+        <h1 className="text-4xl text-center font-semibold mb-8">Register</h1>
+        <form onSubmit={handleSubmit}>
           <input
             onChange={(e) => setName(e.target.value)}
             type="text"
+            className="w-full border border-gray-300 text-white rounded px-3 py-2 mb-4 focus:outline-none focus:border-blue-400 focus:text-black"
             placeholder="Full Name"
+            required
           />
           <input
             onChange={(e) => setEmail(e.target.value)}
             type="text"
+            className="w-full border border-gray-300 text-white rounded px-3 py-2 mb-4 focus:outline-none focus:border-blue-400 focus:text-black"
             placeholder="Email"
+            required
           />
           <input
             onChange={(e) => setPassword(e.target.value)}
             type="password"
+            className="w-full border border-gray-300 text-white rounded px-3 py-2 mb-4 focus:outline-none focus:border-blue-400 focus:text-black"
             placeholder="Password"
+            required
           />
-          <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+          >
             Register
           </button>
-
           {error && (
             <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
               {error}
             </div>
           )}
-
-          <Link className="text-sm mt-3 text-right" href={"/"}>
-            Already have an account? <span className="underline">Login</span>
-          </Link>
         </form>
+        <Link
+          className="block text-center text-blue-500 hover:underline mt-2"
+          href="/"
+        >
+          Already have an account? Login
+        </Link>
       </div>
     </div>
   );
